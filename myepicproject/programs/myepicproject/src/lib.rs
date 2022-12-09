09d1a3753e3c4fd9f454ed08a0b0dead8c493c0b
Francisco <fwmcomputer@gmail.com>
2022-12-10 00:57:48 +0100

    First commit

---
+++ myepicproject/programs/myepicproject/src/lib.rs
@@ -0,0 +1,100 @@
+use anchor_lang::prelude::*;
+//use uuid::Uuid;
+
+declare_id!("3Hp2jWYvcUJqFwuc5YSrB4dCxVZJexcGnpaj3F8QyocD");
+
+#[program]
+pub mod myepicproject {
+    use super::*;
+    pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+        // Get a reference to the account.
+        let base_account = &mut ctx.accounts.base_account;
+        // Initialize total_gifs.
+        base_account.total_gifs = 0;
+        Ok(())
+    }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        // Build the struct.
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+            votes: 0
+        };
+            
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
+
+    /*pub fn add_vote(ctx: Context<AddVote>, gif_link: String) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+
+        base_account.gif_list.iter_mut().find(|gif| )
+
+        // Build the struct.
+        let item = ItemStruct {
+            id: Uuid::new_v4(),
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+            votes: 0
+        };
+            
+        // Add it to the gif_list vector.
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }*/
+}
+
+// Attach certain variables to the StartStuffOff context.
+#[derive(Accounts)]
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+// Specify what data you want in the AddVote Context.
+#[derive(Accounts)]
+pub struct AddVote<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+
+// Create a custom struct for us to work with.
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    //pub id: Uuid,
+    pub gif_link: String,
+    pub user_address: Pubkey,
+    pub votes: u64
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+    // Attach a Vector of type ItemStruct to the account.
+    pub gif_list: Vec<ItemStruct>,
+}
